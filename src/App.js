029324e4d2c5f122fe4ccb6b1b8b9ed9b4e58838
IreAyo <mpossicon@gmail.com>
2020-02-06 02:34:21 +0100

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const [currentQuestion, setCurrentQuestion] = useState(0);
 const [currentAnswer, setCurrentAnswer] = useState("");
 const [answers, setAnswers] = useState([]);
 const [error, setError] = useState(' ');
-
+const [showResults, setShowResults] = useState(false);
   const questions = [
     {
         id: 1,
@@ -48,10 +48,15 @@ const question = questions[currentQuestion];
 
 const handleClick = e =>{
   setCurrentAnswer(e.target.value)
+  setError('');
 }
 
 const next = () => {
-  const answer = {question: question.id, answer: currentAnswer};
+  const answer = {questionId: question.id, answer: currentAnswer};
+  if (!currentAnswer){
+    setError('Pleases select an option');
+    return;
+  }
   answers.push(answer);
   setAnswers(answers);
   setCurrentAnswer('');
@@ -60,6 +65,7 @@ const next = () => {
     setCurrentQuestion(currentQuestion + 1);
     return;
   }
+  setShowResults(true)
 }
 const renderError = () =>{
   if(!error){
@@ -68,7 +74,43 @@ const renderError = () =>{
 return <div className="error">{error}</div>
 }
 
+const renderResultMark = (question, answer) => {
+  if(question.correct_answer === answer.answer){
+    return <span className="correct">Correct</span>
+  }
+  return <span className='failed'>Failed</span>
+}
+ 
+ const renderResultsData = () => {
+   return answers.map(answer => {
+     const question = questions.find(
+        question => question.id === answer.questionId
+     );
+   return <div key={question.id}>{question.question} - {renderResultMark(question, answer)}</div>
+   });
+ };
+ 
+const restart = () =>{
+  setAnswers([]);
+  setCurrentAnswer('');
+  setCurrentQuestion(0);
+  setShowResults(false);
+}
 
+
+
+if (showResults){
+return (
+  <div className="container results">
+    <h2>Results</h2>
+   <ul>{renderResultsData()}</ul>
+    <button className='btn btn-primary' onClick={restart}>
+     Restart
+    </button>
+  </div>
+)
+}
+else {
   return (
     <div className="container">
       <Progress total={questions.length} current={currentQuestion + 1} />
@@ -81,5 +123,5 @@ return <div className="error">{error}</div>
     </div>
   );
 }
-
+}
 export default App;
